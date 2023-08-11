1d9d8a808fef517c5601fa7a4af99354a18f5de5
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-08-11 13:55:37 +0200

    first commit

---
+++ js/game.js
@@ -0,0 +1,9 @@
+let canvas;
+let ctx;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    character.src = '../img/2_character_pepe/2_walk/W-21.png';
+    ctx = canvas.getContext('2d');
+}
