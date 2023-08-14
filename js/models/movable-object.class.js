9ae227903caf2fe80e6db224524bf7b534ea874d
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-08-14 15:58:09 +0200

    Added Background

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -1,8 +1,8 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 220;
     img;
-    height = 100;
+    height = 150;
     width = 100;
 
     loadImage(path) {
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -10,6 +10,13 @@ class World {
         new Cloud(),
     ]
     ctx;
+    canvas;
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+    ];
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
@@ -20,20 +27,27 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Canavas wird gecleart
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height); // Character wird dargestellt
-
-        this.enemies.forEach(enemy => { // Hühner werden dargestellt
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
-        })
-
-        this.clouds.forEach(cloud => { // Hühner werden dargestellt
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height)
-        })
+        this.addObjectsToMap(this.backgroundObjects); // Hintergrund wird dargestellt
+        this.addObjectsToMap(this.clouds); // Wolken werden dargestellt
+        this.addObjectsToMap(this.enemies); // Hühner werden dargestellt
+        this.addToMap(this.character); // Character wird dargestellt
+        
+        
 
         // Draw wird immer wieder aufgerufen
         let self = this;
         requestAnimationFrame(function() {
             self.draw();
         })
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        })
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
     }
 }
