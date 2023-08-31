860564f60089184647a35164f0340c9bdcac0498
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-08-31 18:31:04 +0200

    Added Jumoing abillity

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -9,15 +9,28 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ];
     currentImage = 0;
     world;
     speed = 10;
     walking_sound = new Audio('audio/footsteps.wav');
 
+
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages(this.IMAGES_WALKING)
-
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applyGravity();
         this.animate();
     }
 
@@ -25,24 +38,33 @@ class Character extends MovableObject {
 
         setInterval( () => {
             this.walking_sound.pause(); // Pausiert den Sound da dieser sonst immer bis zum ende durchlaudfen würde
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) { 
+
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) { // Character läuft nach rechts
                 this.x += this.speed // World moving right
                 this.otherDirection = false;
                 this.walking_sound.play(); //Spielt denn walking sound ab
             }
 
-            if (this.world.keyboard.LEFT && this.x > 0) { 
+            if (this.world.keyboard.LEFT && this.x > 0) { //Character läfut nach links
                 this.x -= this.speed // World moving left
                 this.otherDirection = true; // Character turn other direction
                 this.walking_sound.play(); //Spielt denn walking sound ab
             }
 
+            if(this.world.keyboard.UP && this.isOnGround()) {
+                this.speedY = 20;
+                console.log(this.y);
+            }
+
             this.world.camera_x = -this.x + 100; // Gibt die position für die Kamera an
         },1000 / 60)
 
-        setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // Walk animation
+        setInterval(() => { 
+            if(this.isAboveGround()) { // Jump animation wenn der Character springt
+                this.playAnimation(this.IMAGES_JUMPING)
+            }
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { // Walk animation nur wenn der Character läuft
                 this.playAnimation(this.IMAGES_WALKING)
             }
         }, 1000 / 20);
--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -8,13 +8,15 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
-    loadImages(arr) {
+    loadImages(arr) { //Images werden in ein Array gepackt
         arr.forEach((path) => {
             let img = new Image();
             img.src = path;
@@ -37,5 +39,21 @@ class MovableObject {
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
+    }
+
+    applyGravity() { // Fallgeschwindigkeit wird berechnet
+        setInterval(() => {
+            if(this.isAboveGround() || this.speedY > 0)
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+        },1000 / 30)
+    }
+
+    isAboveGround() {
+        return this.y < 220;
+    }
+
+    isOnGround() {
+        return this.y == 220;
     }
 }
