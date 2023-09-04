b530f814ec911410fcaa29bf7ab23eade83a8297
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-09-04 18:39:27 +0200

    Added Idle Animation

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -34,6 +34,18 @@ class Character extends MovableObject {
         'img/2_character_pepe/4_hurt/H-42.png',
         'img/2_character_pepe/4_hurt/H-43.png'
     ];
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
     currentImage = 0;
     world;
     speed = 10;
@@ -46,6 +58,7 @@ class Character extends MovableObject {
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_IDLE);
         this.applyGravity();
         this.animate();
     }
@@ -81,12 +94,15 @@ class Character extends MovableObject {
                 this.playAnimation(this.IMAGES_HURT)
             } else if (this.isAboveGround()) { // Jump animation wenn der Character springt
                 this.playAnimation(this.IMAGES_JUMPING)
+            } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { // Walk animation nur wenn der Character läuft
+                this.playAnimation(this.IMAGES_WALKING)
             } else {
-
-                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { // Walk animation nur wenn der Character läuft
-                    this.playAnimation(this.IMAGES_WALKING)
-                }
+                this.playAnimation(this.IMAGES_IDLE)
             }
+
+
+
+
         }, 1000 / 20);
 
     }
